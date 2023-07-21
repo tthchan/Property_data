ddd67768455675c6bcc065e1c705fa3ddaf2c42d
tthchan <139326079+tthchan@users.noreply.github.com>
2023-07-21 16:22:40 +0800

    Update main.py
    Included more sale data

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import date, datetime, timedelta
-import openpyxl
-from openpyxl import load_workbook
+
+def get_data(url):
+  driver.get(url)
+  data = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div[6]/div/div[1]/div[1]/div/h2/span/span').text
+  return data
+
+def store_sale(data):  # Store scrapped sale data to the list as int
+  if "," in data:
+    data = data.replace(",","")
+  else:
+    pass
+  new_data_sale.append(int(data))
+
+def store_lease(data):  # Store scrapped lease data to the list as int
+  if "," in data:
+    data = data.replace(",","")
+  else:
+    pass
+  new_data_lease.append(int(data))
 
 #set webdriver
 chrome_options = webdriver.chrome.options.Options()
@@ -24,46 +41,42 @@ options.add_argument("safebrowsing-disable-extension-blacklist")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-workbook = load_workbook(filename='Centaline_sale_lease.xlsx')
-sheet = workbook.active
-Date = []
-for cell in sheet.iter_cols(min_row=2, max_row=sheet.max_row, min_col=1, max_col=1):
-    for elem in cell:
-        Date.append(elem.value.date())
-Sale = []
-for cell in sheet.iter_cols(min_row=2, max_row=sheet.max_row, min_col=2, max_col=2, values_only=True):
-    for elem in cell:
-        Sale.append(elem)
-Lease = []
-for cell in sheet.iter_cols(min_row=2, max_row=sheet.max_row, min_col=3, max_col=3,values_only=True):
-    for elem in cell:
-        Lease.append(elem)
+df_sale = pd.read_excel("Centaline_sale_lease.xlsx", sheet_name="Sale")
+df_lease = pd.read_excel("Centaline_sale_lease.xlsx", sheet_name="Lease")
+new_data_sale = []
+new_data_lease = []
 
-#Sale
-driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
-url = 'https://hk.centanet.com/findproperty/list/buy'
-driver.get(url)
-sale = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div[6]/div/div[1]/div[1]/div/h2/span/span').text
-print(sale.replace(",",""))
-Sale.append(int(sale.replace(",","")))
-driver.quit()
-
-#Lease
-driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
-url = 'https://hk.centanet.com/findproperty/list/rent'
-driver.get(url)
-lease = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div[6]/div/div[1]/div[1]/div/h2/span/span').text
-print(lease.replace(",",""))
-Lease.append(int(lease.replace(",","")))
-driver.quit()
-
-#Date
 d = date.today() + timedelta(days=1)
 print(d)
-Date.append(d)
+new_data_sale.append(d)
+new_data_lease.append(d)
 
-df=pd.DataFrame()
-df["日期"] = Date
-df["賣盤"] = Sale
-df["租盤"] = Lease
-df.to_excel("Centaline_sale_lease.xlsx",sheet_name="sheet1", index=False)
+#Sequence: All sale, sale_3m, ..., sale_30m, sale_200ft, ..., sale_500ft)
+driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+sale_links = ["https://hk.centanet.com/findproperty/list/buy", "https://hk.centanet.com/findproperty/list/buy?q=33e606ccc2",
+"https://hk.centanet.com/findproperty/list/buy?q=33e60561e5", "https://hk.centanet.com/findproperty/list/buy?q=33e60576e2",
+"https://hk.centanet.com/findproperty/list/buy?q=33e6055b97", "https://hk.centanet.com/findproperty/list/buy?q=33e61790c0",
+"https://hk.centanet.com/findproperty/list/buy?q=33e6178d32", "https://hk.centanet.com/findproperty/list/buy?q=33e63a2626",
+"https://hk.centanet.com/findproperty/list/buy?q=33e6178b2a", "https://hk.centanet.com/findproperty/list/buy?q=3511ff5d6d",
+"https://hk.centanet.com/findproperty/list/buy?q=33ea2887d9", "https://hk.centanet.com/findproperty/list/buy?q=482827712b",
+"https://hk.centanet.com/findproperty/list/buy?q=33e92a8639", "https://hk.centanet.com/findproperty/list/buy?q=33e7629114",
+"https://hk.centanet.com/findproperty/list/buy?q=33e92a817b", "https://hk.centanet.com/findproperty/list/buy?q=33e7627d5a"]
+for link in sale_links:
+  sale = get_data(link)
+  print(sale)
+  store_sale(sale)
+lease = get_data("https://hk.centanet.com/findproperty/list/rent")
+print(lease)
+store_lease(lease)
+driver.quit()
+
+df_sale.loc[len(df_sale)] = new_data_sale
+df_lease.loc[len(df_lease)] = new_data_lease
+df_sale['日期'] = pd.to_datetime(df_sale['日期'])
+df_sale['日期'] = df_sale['日期'].dt.strftime('%Y-%m-%d')
+df_lease['日期'] = pd.to_datetime(df_lease['日期'])
+df_lease['日期'] = df_lease['日期'].dt.strftime('%Y-%m-%d')
+print(df_sale)
+with pd.ExcelWriter("Centaline_sale_lease.xlsx") as writer:
+  df_sale.to_excel(writer, sheet_name="Sale", index=False)
+  df_lease.to_excel(writer, sheet_name="Lease", index=False)
